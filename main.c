4a4eeacc00813f0eeaa35fb1f90341834f4ed58e
Alexandre Harder <aharder@c1r7s5.42luxembourg.lu>
2025-02-07 14:44:36 +0100

    not working just understanding the logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aharder <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:05:13 by aharder           #+#    #+#             */
-/*   Updated: 2025/02/02 18:35:44 by aharder          ###   ########.fr       */
+/*   Updated: 2025/02/07 14:44:11 by aharder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,41 @@ int	checkargs(int argc, char **argv)
 		return (0);
 }
 
-int	createchildren(int argc, char **argv, char **envp)
+void	executecommand(char *cmd, char **args, char **envp)
 {
+	int	fd[2];
 	pid_t	p;
 
+	pipe(fd);
 	p = fork();
 	if (p == 0)
 	{
-		// CODE
-	}	
+		close(fd[0]);
+		dup2(fd[1], 1);
+		execve(ft_strjoin("/bin/", cmd), args, envp);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], 0);
+		waitpid(p, NULL, 0);
+	}
 }
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	int	fd[2];
+	int	i;
+	char	***args;
 
-	if (checkargs(argc, argv)
+	if (checkargs(argc, argv))
 		return (print_error("args"));
-	pipe(fd);
-	createchildren(argc, argv, envp);
+	args = malloc((argc + 1) * sizeof(**args));
+	i = 2;
+	while (i < argc)
+	{
+		args[i] = ft_split(argv[i], " ");
+		executecommand(args[i][0], &args[i][1], envp);
+		i++;
+	}
+	free_triple(args);
 }
