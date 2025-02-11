d09865eb60b711c6444b5ebb0e438706b33d8bd6
paraponerashinji <aharder@student.42luxembourg.lu>
2025-02-11 18:21:50 +0100

    not finished at all but im doing something

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aharder <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:05:13 by aharder           #+#    #+#             */
-/*   Updated: 2025/02/07 14:44:11 by aharder          ###   ########.fr       */
+/*   Updated: 2025/02/11 18:15:58 by aharder          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	checkargs(int argc, char **argv)
 		return (0);
 }
 
-void	executecommand(char *cmd, char **args, char **envp)
+void	executecommand(int *inout_fd, char *cmd, char **args, char **envp)
 {
 	int	fd[2];
 	pid_t	p;
@@ -26,31 +26,47 @@ void	executecommand(char *cmd, char **args, char **envp)
 	if (p == 0)
 	{
 		close(fd[0]);
-		dup2(fd[1], 1);
-		execve(ft_strjoin("/bin/", cmd), args, envp);
+		dup2(inout_fd[0], STDIN_FILENO)
+		dup2(fd[1], STDOUT_FILENO);
+		full_cmd = ft_strjoin("/bin/", cmd);
+		execve(full_cmd, args, envp);
+		free(full_cmd);
 	}
 	else
 	{
 		close(fd[1]);
-		dup2(fd[0], 0);
+		dup2(fd[0], inout_fd[1]);
 		waitpid(p, NULL, 0);
 	}
 }
 
+void	createpipes(int	argc, char **argv, char **envp)
+{
+	int	inout_fd[2];
+	int	i;
+	char	**args;
+
+	if (access(argv[1], F_OK) == 0)
+		inout_fd[0] = open(argv[1], O_RDONLY);
+	if (access(argv[argc - 1], F_OK) == 0)
+		inout_fd[1] = open(argv[argc - 1], O_WRONLY);
+	i = 2;
+	while (i < argc - 1)
+	{
+		args = ft_split(argv[i], " ");
+		executecommand(inout_fd, args[0], args, envp);
+		free(args);
+		i++;
+	}
+}
 int	main(int argc, char *argv[], char *envp[])
 {
 	int	i;
 	char	***args;
-
+	char	*infile;
+	char	*outfile;
 	if (checkargs(argc, argv))
 		return (print_error("args"));
-	args = malloc((argc + 1) * sizeof(**args));
-	i = 2;
-	while (i < argc)
-	{
-		args[i] = ft_split(argv[i], " ");
-		executecommand(args[i][0], &args[i][1], envp);
-		i++;
-	}
+	createpipes(argc, argv, envp);
 	free_triple(args);
 }
